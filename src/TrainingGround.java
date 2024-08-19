8f8e1c2d81e294dc1f44bab5ddd00d09ea7b60d6
ArgunX <samooborona88@gmail.com>
2024-08-19 09:58:34 +0300

    выполнен пункт 1 задания

---
+++ src/TrainingGround.java
@@ -0,0 +1,9 @@
+import model.Hero;
+
+public class TrainingGround {
+    public static void main(String[] args) {
+        Hero hero = new Hero("ТОР");
+        hero.attackEnemy();
+
+    }
+}
---
+++ src/model/Hero.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Hero {
+    private String name;
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void attackEnemy(){
+        System.out.println("Герой" + " " + name + " " + "атакует врага");
+    }
+}
