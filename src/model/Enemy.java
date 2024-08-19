462f90bddf7cf9cc1b3802a2f39cab6f63f4b9eb
ArgunX <samooborona88@gmail.com>
2024-08-19 10:35:29 +0300

    выполнен пункт 5 задания

--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -1,6 +1,6 @@
 package model;
 
-public class Enemy {
+public class Enemy implements Mortal {
     private int health;
 
     public Enemy(int health) {
@@ -17,5 +17,10 @@ public class Enemy {
     public void takeDamage(int damage){
         health = getHealth()- damage;
         System.out.println("Здоровье врага" + " " + health);
+    }
+
+    @Override
+    public boolean isAlive() {
+        return health > 0;
     }
 }
